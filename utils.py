69e65cc402f76d3f8fc74033794844bd7db95779
Mel <132060432+MelProgramming@users.noreply.github.com>
2025-11-29 04:12:51 -0800

    Add files via upload
    Version 2 upload

---
+++ utils.py
@@ -0,0 +1,12 @@
+import re
+
+def clean_text(text):
+    """
+    Clean tweet text by removing links, mentions, and special characters.
+    """
+
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"@\w+", text)
+    text = re.sub(r"#", "", text)
+    text = re.sub(r"[^A-Za-z0-9\s]", "", text)
+    return text.strip()
