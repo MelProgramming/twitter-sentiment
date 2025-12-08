3fd29835b458fa7f7cfb02ab1307769e5d2fe280
Mel <132060432+MelProgramming@users.noreply.github.com>
2025-12-07 18:20:24 -0800

    Bug fix clean_text function in utils.py
    Missing argument on line 9

--- utils.py
+++ utils.py
@@ -6,7 +6,8 @@ def clean_text(text):
     """
 
     text = re.sub(r"http\S+", "", text)
-    text = re.sub(r"@\w+", text)
+    text = re.sub(r"@\w+", "", text)
     text = re.sub(r"#", "", text)
     text = re.sub(r"[^A-Za-z0-9\s]", "", text)
+
     return text.strip()
